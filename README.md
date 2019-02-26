b7249347988118194a77446698880158b09d81b4
Toshio Ito <debug.ito@gmail.com>
2019-02-26 21:26:09 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# app-income-items
+
+
+## Author
+
+Toshio Ito <debug.ito@gmail.com>
---
+++ src/App/Income/Items.hs
@@ -0,0 +1,8 @@
+-- |
+-- Module: App.Income.Items
+-- Description:
+-- Maintainer: Toshio Ito <debug.ito@gmail.com>
+-- 
+module App.Income.Items
+       () where
+
